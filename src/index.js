ae5baba669fcbbba56fd88caff6946c5c47b9bdd
Andriy <agolumbovsky@gmail.com>
2022-05-11 10:47:45 -0400

    setState to X-gonnagiveittoya

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      value: "Dick",
     };
   }
   render() {
     return (
-      <button className="square" onClick={() => console.log("click")}>
-        {this.props.value}
+      <button className="square" onClick={() => {this.setState({value: "X"})}}>
+        {this.state.value}
       </button>
     );
   }
